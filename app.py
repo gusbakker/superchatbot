6abdb20f9de60652c2e4a44331171226e58fb396
gusbakker <gustavobakker@hotmail.com>
2021-04-24 21:32:36 +0100

    cenas

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import environ
 
-app = Flask(__name__)
-MONGODB_NAME = ""
-MONGODB_URI = ""
+ROOT_DIR = (
+        environ.Path(__file__)
+)
+APPS_DIR = ROOT_DIR.path("superchatbot")
+env = environ.Env()
+
+MONGODB_NAME = env("MONGODB_NAME")
+MONGODB_URI = env("MONGODB_URI")
+
 english_bot = ChatBot("English Bot",
                      storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
                      database = MONGODB_NAME,
@@ -12,6 +19,7 @@ english_bot = ChatBot("English Bot",
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
 
+app = Flask(__name__)
 
 @app.route("/")
 def home():
