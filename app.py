8e27d4e0e5e9424675bbef4c39f487b421f13890
gusbakker <gustavobakker@hotmail.com>
2021-04-24 18:25:37 +0100

    added Chatterbot library and new design

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
 from flask import Flask, render_template, request
-#from chatterbot import ChatBot
-#from chatterbot.trainers import ChatterBotCorpusTrainer
-#from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-#bot = ChatBot("Candice")
-#bot.set_trainer(ListTrainer)
-#bot.train(['What is your name?', 'My name is Candice'])
-#bot.train(['Who are you?', 'I am a bot' ])
-#bot.train(['Do created you?', 'Tony Stark', 'Sahil Rajput', 'You?'])
-#bot.set_trainer(ChatterBotCorpusTrainer)
-#bot.train("chatterbot.corpus.english")
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
+
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return processText(userText)
-    #return str(bot.get_response(userText))
+    user_text = request.args.get('msg')
+    return str(english_bot.get_response(user_text))
 
-def processText(userText):
-    if(userText=='hi'):
-        return 'fuck you'
-    else:
-        return 'lol'
 
 if __name__ == "__main__":
     app.run()
